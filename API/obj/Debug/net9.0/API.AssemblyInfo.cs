9a41020098c0ac24cd628169bc92716d53852718
KwanTNg <128802089+KwanTNg@users.noreply.github.com>
2025-02-27 11:56:04 +0000

    add readme

--- API/obj/Debug/net9.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net9.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3d6489eb1093cf84026b08478f06df286fd8c54f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2aad5cc1112eecb7045c110ba8380769d5f1a0a9")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
