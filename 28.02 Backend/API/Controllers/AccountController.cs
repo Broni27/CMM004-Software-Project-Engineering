5e47b6261c311fa3788c338bff20077a8c780ada
Broni27 <broni.27@mail.ru>
2025-03-02 19:17:20 +0000

    Front and Back
    First Push to Main

---
+++ 28.02 Backend/API/Controllers/AccountController.cs
@@ -0,0 +1,113 @@
+using API.DTOs;
+using API.models;
+using API.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Security.Cryptography;
+using System.Security.Claims;
+using System.Text;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        private readonly DataContext _context;
+        private readonly TokenService _tokenService;
+        public AccountController(DataContext context, TokenService tokenService)
+        {
+            _context = context;
+            _tokenService = tokenService;
+        }
+
+        [HttpPost("login")] //api/account/login
+        public async Task<ActionResult<UserDto>> Login(LoginDto loginDto)
+        {
+            var user = await _context.Users.FirstOrDefaultAsync(x => x.Email.ToLower() == loginDto.Email.ToLower());
+
+            if (user == null)
+                return Unauthorized("Invalid email or password");
+
+            var passwordSalt = new HMACSHA512(user.PasswordSalt);
+            var computeHash = passwordSalt.ComputeHash(Encoding.UTF8.GetBytes(loginDto.Password));
+
+            if (!computeHash.SequenceEqual(user.PasswordHash))
+                return Unauthorized("Invalid email or password");
+
+            return new UserDto
+            {
+                Username = user.Username,
+                Email = user.Email,
+                Realname = user.Realname,
+                Token = _tokenService.CreateToken(user)
+            };
+        }
+
+        [HttpPost("register")] //api/account/register
+        public async Task<ActionResult<UserDto>> Register(RegisterDto registerDto)
+        {
+            if (await _context.Users.AnyAsync(u => u.Username.ToLower() == registerDto.Username.ToLower()))
+                return BadRequest("This username is used!");
+            if (await _context.Users.AnyAsync(u => u.Email.ToLower() == registerDto.Email.ToLower()))
+                return BadRequest("This email is used!");
+            var signingKey = new HMACSHA512();
+            User newUser = new User
+            {
+                Username = registerDto.Username.ToLower(),
+                Email = registerDto.Email,
+                Realname = registerDto.Realname,
+                Role = Role.USER,
+                PasswordHash = signingKey.ComputeHash(Encoding.UTF8.GetBytes(registerDto.Password)),
+                PasswordSalt = signingKey.Key
+            };
+            _context.Users.Add(newUser);
+            await _context.SaveChangesAsync();
+            return new UserDto
+            {
+                Username = newUser.Username,
+                Email = newUser.Email,
+                Realname = newUser.Realname,
+                Token = _tokenService.CreateToken(newUser)
+            };
+        }
+
+        // User profile endpoint, allows retrieval of user details on Profile page
+        [HttpGet("profile")] //api/acount/profile
+        [Authorize]
+        public async Task<ActionResult<UserDto>> GetProfile()
+        {
+            //Retrieve token from the Authorization header
+            var token = Request.Headers["Authorization"].ToString();
+
+            //Logs token for debugging
+            Console.WriteLine($"Received Token: {token}");
+            
+            //Retrieve user from JWT Token
+            var userId = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+
+            if (string.IsNullOrEmpty(userId))
+            {
+                return Unauthorized("User not authenticated");
+            }
+
+            //Finds user by ID
+            var user = await _context.Users.FirstOrDefaultAsync(x => x.Id.ToString() == userId);
+
+            if (user == null)
+            {
+                return Unauthorized("User not found");
+            }
+
+            //Returns user profile data
+            return new UserDto
+            {
+                Username = user.Username,
+                Realname = user.Realname,
+                Email = user.Email,
+                Token = _tokenService.CreateToken(user)
+            };
+        }
+    }
+}
--- Page1-2/app.js
+++
@@ -1,7 +0,0 @@
-function redirectToPage(url) {
-  window.location.href = url;
-}
-
-function redirectToHome(url) {
-  window.location.href = url;
-}
