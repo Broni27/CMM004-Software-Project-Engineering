a49230c2a167b179f620de7ae81d2e394dbc55dd
Oliwer Szmytkowski <oliwers2002@gmail.com>
2025-03-06 21:48:58 +0000

    06.03 Backend Update
    AccountController.cs changed
    EventController.cs changed

--- 06.03 Backend/API/Program.cs
+++ 06.03 Backend/API/Program.cs
@@ -7,7 +7,7 @@ using API.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+//Add services to the container.
 builder.Services.AddControllers();
 
 //Setting up DbContext
