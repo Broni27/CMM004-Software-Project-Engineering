150db41e53d775560c66cf0172a6204f7c08db9b
Oliwer Szmytkowski <oliwers2002@gmail.com>
2025-03-06 21:49:21 +0000

    .git changes

--- 28.02 Backend/API/obj/Debug/net9.0/API.AssemblyInfo.cs
+++ 28.02 Backend/API/obj/Debug/net9.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+997e58f764b8816a30d7e3f6798b8f95995e9d68")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3f70c6ff725b1621955599c64f6ef9df26dacdec")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
